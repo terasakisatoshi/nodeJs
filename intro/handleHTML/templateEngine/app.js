835ea992e7cd680a3762ab558b5c969a079e1d11
SatoshiTeraaki <terasakisatoshi.math@gmail.com>
2017-12-29 10:24:50 +0900

    add

---
+++ intro/handleHTML/templateEngine/app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const fs = require("fs");
+const ejs = require("ejs");
+
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
+
+function getFromClient(request, response) {
+    var content = ejs.render(index_page);
+    response.writeHead(200, {
+        'Contenty-Type': 'text/html'
+    });
+    response.write(content);
+    response.end();
+}
+var server = http.createServer(getFromClient);
+
+server.listen(3000);
+console.log('Server Start!');

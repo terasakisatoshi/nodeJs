b9c0e13a8efbd28c7a7dfee0f97ff01f9a3f0b17
SatoshiTeraaki <terasakisatoshi.math@gmail.com>
2017-12-31 10:52:16 +0900

    add code

---
+++ intro/handleHTML/templateEngine/part2/app.js
@@ -0,0 +1,20 @@
+const http=require('http');
+const fs=require('fs');
+const ejs=require('ejs');
+
+const index_page = fs.readFileSync('./index.ejs','utf8');
+
+function getFromClient(request,response) {
+    var content = ejs.render(index_page, {
+        title:"Index ページ",
+        content:"これはテンプレートを使ったサンプルページです",
+    });
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.write(content);
+    response.end();
+};
+
+var server = http.createServer(getFromClient);
+
+server.listen(3000);
+console.log('Server start!!!');

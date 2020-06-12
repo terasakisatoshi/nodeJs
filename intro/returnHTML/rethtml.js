e769ff40dd6b07b2b1ed355d72baca30c9ed7ebb
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-06-12 15:22:41 +0900

    delete old assets

--- intro/returnHTML/rethtml.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-var server = http.createServer(
-    (req, res) => {
-        res.end("<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>");
-    }
-);
-
-server.listen(3000);
